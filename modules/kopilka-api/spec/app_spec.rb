279676be7115d8f5ca2b24f960510a91116cc7f5
Nerbyk <danil.maximov@flatstack.com>
2022-05-29 18:59:33 +0200

    change return format of app handler

--- modules/kopilka-api/spec/app_spec.rb
+++ modules/kopilka-api/spec/app_spec.rb
@@ -5,7 +5,7 @@ describe App::Handler do
   describe '#process' do
     it 'returns a greeting' do
       event = { 'data' => { 'name' => 'World' } }
-      assert_equal 'Hello, World, I\'m from App::Handler', App::Handler.process(event: event)
+      assert_equal({ statusCode: 200, body: 'Hello, World, I\'m from App::Handler' }, App::Handler.process(event: event))
     end
   end
 end
