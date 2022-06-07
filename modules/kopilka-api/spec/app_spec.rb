0c74e2c255a13a866141482c956b81a30a5c18f1
Nerbyk <danil.maximov@flatstack.com>
2022-06-07 15:16:18 +0200

    add steep type checker, & improve rake experience

--- modules/kopilka-api/spec/app_spec.rb
+++ modules/kopilka-api/spec/app_spec.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'test_helper'
 require './lib/app'
 
@@ -5,7 +7,8 @@ describe App::Handler do
   describe '#process' do
     it 'returns a greeting' do
       event = { 'data' => { 'name' => 'World' } }
-      assert_equal({ statusCode: 200, body: 'Hello, World, I\'m from App::Handler' }, App::Handler.process(event: event))
+      assert_equal({ statusCode: 200, body: 'Hello, World, I\'m from App::Handler' },
+                   App::Handler.process(event: event))
     end
   end
 end
