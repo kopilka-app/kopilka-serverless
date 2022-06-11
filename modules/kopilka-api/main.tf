6e22b775313f728132f3629784b995e1957592e7
Nerbyk <danil.maximov@flatstack.com>
2022-06-11 04:26:10 +0200

    add simple infrastructure code

--- modules/kopilka-api/main.tf
+++ modules/kopilka-api/main.tf
@@ -1,29 +1,11 @@
 variable "global_name" { default = "kopilka-api" }
+locals {
+  endpoints = toset(["list_kopilka", "show_kopilka", "create_kopilka", "update_kopilka", "destroy_kopilka"])
+}
 
 provider "aws" {
   region = "eu-central-1"
-  profile = "default"
 }
-
-data "archive_file" "lambda_zip" {
-  type        = "zip"
-  source_dir = "./lib"
-  output_path = "./builds/lambda"
-}
-
-resource "aws_lambda_function" "lambda" {
-  filename      = data.archive_file.lambda_zip.output_path
-  function_name = var.global_name
-  role          = aws_iam_role.lambda.arn
-
-  source_code_hash = filebase64sha256(data.archive_file.lambda_zip.output_path)
-  memory_size      = 128
-  timeout          = 60
-
-  runtime = "ruby2.7"
-  handler = "app.App::Handler.process"
-}
-
 resource "aws_iam_role" "lambda" {
   name = var.global_name
 
@@ -42,4 +24,42 @@ resource "aws_iam_role" "lambda" {
       ]
     }
   POLICY
+}
+
+data "archive_file" "lambda_zip" {
+  for_each = local.endpoints
+
+  type = "zip"
+  source_dir = "./dist/${each.key}"
+  output_path = "./builds/${each.key}.zip"
+}
+
+resource "aws_lambda_function" "kopilka_api" {
+  for_each      = local.endpoints
+  function_name = each.key
+  role          = aws_iam_role.lambda.arn
+
+  filename      = "./builds/${each.key}.zip"
+  handler = "lambda_handlers/${each.key}/handler.KopilkaApi::LambdaHandler.call"
+  runtime = "ruby2.7"
+  
+  source_code_hash = filebase64sha256("./builds/${each.key}.zip")
+  memory_size      = 128
+  timeout          = 60
+}
+
+
+resource "aws_lambda_function_url" "lambda_url" {
+  for_each = aws_lambda_function.kopilka_api
+  function_name      = each.value.function_name
+  authorization_type = "NONE"
+
+  cors {
+    allow_credentials = true
+    allow_origins     = ["*"]
+    allow_methods     = ["GET"]
+    allow_headers     = ["date", "keep-alive"]
+    expose_headers    = ["keep-alive", "date"]
+    max_age           = 86400
+  }
 }
